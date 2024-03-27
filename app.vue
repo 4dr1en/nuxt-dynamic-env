46b1308b2ef264f1a4655a14d47433ff484c1b5d
adrien charrier <adrien.charrier@outlook.fr>
2024-03-27 16:36:15 +0100

    init

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+  export default defineNuxtComponent({
+    name: 'App',
+    mounted() {
+      console.log(this.$config.public.msg)
+    }
+  })
+</script>
+
+<template>
+  <div>
+    {{ $config.public.msg }}
+  </div>
+</template>
+
+<style lang="scss"></style>
